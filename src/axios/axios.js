423a74bf984ab0ad5c30b994b95328ac1623986c
abinand <abinandsathyan532gmail.com>
2023-10-24 19:25:21 +0530

    host change

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'https://prodectlist.onrender.com',
+    baseURL:'https://productbackend-8m20.onrender.com/',
     // baseURL:'http://localhost:3000/',
     headers: {
         "Content-Type": "application/json",
